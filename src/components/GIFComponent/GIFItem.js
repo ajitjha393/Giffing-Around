05c94e28e90cc5db314ab4e0769a4b7e0b0a222d
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-23 18:05:22 +0530

    Rendered Gifs

--- src/components/GIFComponent/GIFItem.js
+++ src/components/GIFComponent/GIFItem.js
@@ -1,5 +1,5 @@
-function GIFItem() {
-	return <div></div>
+function GIFItem({ gifUrl }) {
+	return <img src={gifUrl} alt="Funny Giffy kebab" />
 }
 
 export default GIFItem
--- src/components/GIFComponent/GIFList.js
+++ src/components/GIFComponent/GIFList.js
@@ -1,20 +1,48 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
+
+import GIFItem from './GIFItem'
 
 const BASE_URL = 'https://api.giphy.com/v1'
 
+const tags = [
+	'cola',
+	'kebab',
+	'dog',
+	'happy',
+	'meme',
+	'cat',
+	'cricket',
+	'dance',
+	'wasps',
+]
+
+function getRandomTag() {
+	const randomIndex = Math.floor(Math.random() * tags.length)
+	return tags[randomIndex]
+}
+
 function GIFList() {
+	const [gifs, setGifs] = useState([])
+
 	useEffect(() => {
 		const getRandomGIF = async () => {
+			const randomTag = getRandomTag()
+			console.log(randomTag)
+
 			const res = await axios.get(
-				`${BASE_URL}/gifs/random?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&rating=G`
+				`${BASE_URL}/gifs/random?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&rating=G&tag=${randomTag}`
 			)
+
 			console.log(res)
+			const gifUrl = res.data.data.image_url
+			console.log(gifUrl)
+			setGifs(prevgifs => prevgifs.concat(gifUrl))
 		}
 		getRandomGIF()
-	})
+	}, [])
 
-	return <div></div>
+	return <div>{gifs.length && gifs.map(gif => <GIFItem gifUrl={gif} />)}</div>
 }
 
 export default GIFList
