45093e465602d86a6fdcf8c733f1ff35d5d1669b
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-23 16:53:03 +0530

    Added GifList to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MainNav from './components/MainNav/MainNav'
+import GIFList from './components/GIFComponent/GIFList'
 import './App.css'
 
 function App() {
 	return (
 		<section>
 			<MainNav />
+			<GIFList />
 		</section>
 	)
 }
