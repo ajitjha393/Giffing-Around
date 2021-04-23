a1990751e4fa256014e2737a1819c534628cbc46
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-23 21:52:56 +0530

    Added Classifier to gify on Load

--- src/components/GIFComponent/GIFItem.js
+++ src/components/GIFComponent/GIFItem.js
@@ -1,4 +1,18 @@
-function GIFItem({ gifUrl, index }) {
+import { useEffect } from 'react'
+
+function GIFItem({ gifUrl, index, classifier }) {
+	useEffect(() => {
+		if (!classifier) {
+			const ImageElement = document.getElementById('gif-' + index)
+			ImageElement.addEventListener('load', () => {
+				classifier.classify(imageElement, (err, results) => {
+					const labels = getLabels(results)
+					console.log(labels)
+				})
+			})
+		}
+	}, [classifier])
+
 	return <img id={'gif-' + index} src={gifUrl} alt="Funny Giffy kebab" />
 }
 
