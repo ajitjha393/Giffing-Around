4bceaf4f297a6a2f925a4b7b8a43f1bebcde69bd
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-23 17:27:23 +0530

    Fetching Random Gif

--- src/components/GIFComponent/GIFList.js
+++ src/components/GIFComponent/GIFList.js
@@ -1,12 +1,14 @@
 import { useEffect } from 'react'
 import axios from 'axios'
 
-const GIPHY_URL = `https://api.giphy.com/v1/randomid?api_key=&${process.env.REACT_APP_GIPHY_API_KEY}rating=G`
+const BASE_URL = 'https://api.giphy.com/v1'
 
 function GIFList() {
 	useEffect(() => {
 		const getRandomGIF = async () => {
-			const res = await axios.get(GIPHY_URL)
+			const res = await axios.get(
+				`${BASE_URL}/gifs/random?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&rating=G`
+			)
 			console.log(res)
 		}
 		getRandomGIF()
